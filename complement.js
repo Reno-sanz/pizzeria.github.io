832220858e6fe6457bf906f44aac0b7dcb541a80
Renato <reno44monstercat11@gmail.com>
2021-02-12 17:13:45 -0600

    comit

---
+++ complement.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+	sr.reveal('.navbar', {
+		duration: 2000,
+	})
+
+	sr.reveal('.con_title', {
+		duration: 2000,
+
+	})
+
+	sr.reveal('.', {
+		duration: 2000,
+		origin: 'right'
+	})
+
+	sr.reveal('.', {
+		duration: 2000,
+		origin: 'right'
+	})
+
+	sr.reveal('.', {
+		duration: 2000,
+		origin: 'right'
+	})
